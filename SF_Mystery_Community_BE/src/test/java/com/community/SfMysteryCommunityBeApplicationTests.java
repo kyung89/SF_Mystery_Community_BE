4dce78c2c29fc2610a4188eab21018cc9d1db693
kyung <bamtori1029@gmail.com>
2024-09-07 23:12:45 +0900

    initial commit

---
+++ SF_Mystery_Community_BE/src/test/java/com/community/SfMysteryCommunityBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfMysteryCommunityBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
